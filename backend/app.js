8eddabd5fc60f7fce2fa44c485fbed2bf80da5a9
Cagritrkmen <cagritrkmn33@gmail.com>
2024-03-02 20:18:58 +0300

    add models

--- backend/app.js
+++ backend/app.js
@@ -2,7 +2,8 @@ import express from "express";
 import dotenv from "dotenv";
 import cors from "cors"
 import { dbConnection } from "./database/dbConnection.js";
-
+import { errorMiddleware } from "./error/error.js";
+import ReservationRouter from "./routes/reservationRoute.js"
 
 const app = express()
 dotenv.config({path: "./config/config.env"});
@@ -14,6 +15,9 @@ app.use(cors({
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true}));
+app.use("/api/v1/reservation", ReservationRouter)
 dbConnection();
+
+app.use(errorMiddleware);
 
 export default app;
