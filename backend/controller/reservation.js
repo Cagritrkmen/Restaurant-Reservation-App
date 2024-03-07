1674785f580da54686e91b39ef433a77858db29d
Cagritrkmen <cagritrkmn33@gmail.com>
2024-03-07 21:33:27 +0300

    end backend

--- backend/controller/reservation.js
+++ backend/controller/reservation.js
@@ -4,30 +4,34 @@ import { Reservation } from "../models/reservationSchema.js";
 export const sendReservation = async (req, res, next) => {
     const { firstName, lastName, email, phone, date, time } = req.body;
     
-    if (!firstName || !lastName || !email || !phone || !date || !time) {
+    console.log("Request body:", req.body);
+
+    if (!firstName || !lastName || !email || !date || !time || !phone) {
         return next(new ErrorHandler("Please fill out the full reservation form!", 400));
     }
 
     try {
-        await Reservation.create(
+        const reservation = await Reservation.create({
             firstName,
             lastName,
             email,
             phone,
             date,
             time
-        );
+        });
+
+        console.log("Created reservation:", reservation);
 
         res.status(200).json({
             success: true,
             message: "Reservation Sent Successfully!"
         });
     } catch (error) {
+        console.error("Error:", error);
         if (error.name === "ValidationError") {
             const validationErrors = Object.values(error.errors).map(err => err.message);
             return next(new ErrorHandler(validationErrors.join(", "), 400));
-        } else {
-            return next(error);
         }
+        return next(error)
     }
 };
