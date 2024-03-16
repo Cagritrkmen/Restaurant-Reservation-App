a1027d6a1fa2fe19dae00f86da6d9e64e153dcc6
Cagritrkmen <cagritrkmn33@gmail.com>
2024-03-17 00:49:22 +0300

    add some images

--- frontend/src/Pages/Home.jsx
+++ frontend/src/Pages/Home.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import HeroSection from '../components/HeroSection'
 
 const Home = () => {
   return (
-    <h1>HOMEOAGE</h1>
+   <>
+   <HeroSection/>
+   </>
+
   )
 }
 
