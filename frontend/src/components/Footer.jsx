f739d119bdda9e9eb00fc967e3e801f0b8c0ed34
Cagritrkmen <cagritrkmn33@gmail.com>
2024-03-20 23:59:02 +0300

    add readme

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -5,7 +5,7 @@ const Footer = () => {
     <footer>
       <div className="container">
         <div className="banner">
-          <div className="left">ZEESHU</div>
+          <div className="left">CAGRİ</div>
           <div className="right">
             <p>Z6 Gulshan-e-Maymar, Karachi</p>
             <p>Open: 05:00 PM - 12:00 AM</p>
@@ -13,10 +13,10 @@ const Footer = () => {
         </div>
         <div className="banner">
           <div className="left">
-            <p>Developed By CODEWITHZEESHU</p>
+            <p>Developed By CAGRİ TURKMEN</p>
           </div>
           <div className="right">
-            <p>All Rights Reserved By CodeWithZeeshu.</p>
+            <p>All Rights Reserved By CAGRİ.</p>
           </div>
         </div>
       </div>
